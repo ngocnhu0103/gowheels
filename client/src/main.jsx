ff25f4789023d3fc9b897211f7935b7fa1c37353
ngocnhu0103 <ngocnhu010301@gmail.com>
2023-06-26 16:22:27 +0700

    add redux-persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 
 import { Provider } from 'react-redux'
-import { store } from './store/store'
+import { store, persistor } from './store/store'
+import { PersistGate } from 'redux-persist/integration/react';
 import { RouterProvider } from 'react-router-dom'
 import router from './router'
 import Toast from './components/Toast'
@@ -11,8 +12,11 @@ import Toast from './components/Toast'
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <Provider store={store}>
-    <RouterProvider router={router} />
-    <Toast />
+    <PersistGate loading={null} persistor={persistor}>
+      <RouterProvider router={router} />
+      <Toast />
+    </PersistGate>
+
   </Provider>
   // </React.StrictMode>,
 )
--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,10 +1,32 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+import thunk from 'redux-thunk';
+// Slice
 import userSlice from './userSlice'
 import bikeSlice from './bikeSlice'
 import authSlice from './authSlice'
 import toastSlice from './toastSlice'
 
+const persistConfig = {
+    key: 'root',
+    storage, // cookies, sesseion
+}
+
+const rootReducer = combineReducers({
+    'user': userSlice,
+    'bike': bikeSlice,
+    'auth': authSlice,
+    'toast': toastSlice,
+})
+
+const persistedReducer = persistReducer(
+    persistConfig, rootReducer
+)
 
 export const store = configureStore({
-    reducer: { 'user': userSlice, 'bike': bikeSlice, 'auth': authSlice, 'toast': toastSlice },
+    reducer: persistedReducer,
+    middleware: [thunk]
 })
+
+export const persistor = persistStore(store)
