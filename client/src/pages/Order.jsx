9cd2c249212695549198b6140a137ae877e259b3
ngocnhu0103 <ngocnhu010301@gmail.com>
2023-08-29 16:01:49 +0700

    don hang

--- client/src/pages/Order.jsx
+++ client/src/pages/Order.jsx
@@ -1,8 +1,38 @@
+import { useState } from "react"
 
 function Order() {
+    const [tab, setTab] = useState('1')
     return (
         <div>
-            <h1 className='text-center text-3xl font-banner text-primary pb-5'>Đơn hàng</h1>
+            <h1 className='text-center text-3xl font-banner text-primary pb-5'>Quản lý Đơn hàng</h1>
+            <div className="grid grid-cols-5 text-center bg-gray-200">
+                <p onClick={() => {
+                    setTab('1')
+                }} className={`relative cursor-pointer font-semibold hover:text-primary py-2 
+                ${tab === '1' ? 'bg-gray-400/50 text-gray-800' : ' text-gray-500'}
+                `}>Chờ duyệt</p>
+                <p onClick={() => {
+                    setTab('2')
+                }} className={`relative cursor-pointer font-semibold hover:text-primary py-2 
+                ${tab === '2' ? 'bg-gray-400/50 text-gray-800' : ' text-gray-500'}
+                `}>Chờ bàn giao</p>
+                <p onClick={() => {
+                    setTab('3')
+                }} className={`relative cursor-pointer font-semibold hover:text-primary py-2 
+                ${tab === '3' ? 'bg-gray-400/50 text-gray-800' : ' text-gray-500'}
+                `}>Đã bàn giao</p>
+                <p onClick={() => {
+                    setTab('4')
+                }} className={`relative cursor-pointer font-semibold hover:text-primary py-2 
+                ${tab === '4' ? 'bg-gray-400/50 text-gray-800' : ' text-gray-500'}
+                `}>Hoàn thành</p>
+                <p onClick={() => {
+                    setTab('5')
+                }} className={`py-2 cursor-pointer font-semibold hover:text-primary text-gray-500
+                ${tab === '5' ? 'bg-gray-400/50 text-gray-800' : ' text-gray-500'}
+                `}>Đã hủy</p>
+
+            </div>
 
         </div>
     )
